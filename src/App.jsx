769a07605441c24d16c4fee47c1ebb620d8fd7f0
UmSamu <samuel.silva@sptech.school>
2024-04-11 16:18:23 -0300

    Revert "Merge com samu-no-react"
    This reverts commit 9b71f145d787308407cf97aa9000958b9d0798a1.

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,10 @@
-export function App() {
+import SiteInstitucional from "@pages/Site-institucional/site-institucional.jsx";
+
+function App() {
   return (
-    <div className="max-w-min mx-auto h- py-8 flex flex-col flex-1 gap-5 justify-center bg-[FF00FF} text-nowrap">
-      <h1 className="font-mavenPro font-bold text-[90px] text-center text-[#519747]">
-        Bem-vindo(a) de volta!
-      </h1>
-
-      <div className="text-2xl text-right flex flex-col gap-5">
-        <p>Bem-vindo de volta! Estamos felizes em tê-lo conosco novamente.</p>
-        <p className="text-wrap">
-          Faça login para acessar sua conta e explorar todas as novidades que
-          preparamos para você.
-        </p>
-      </div>
-
-      <div className="w-full py-8 flex flex-col gap-8 ">
-        <div className="flex flex-col gap-4">
-          <span className="max-w-min font-mavenPro text-2xl border-b text-[#5EAF6B]">
-            E-MAIL OU NICKNAME:
-          </span>
-          <input
-            className="w-full px-3 w-72 py-6 bg-[#111111]/85 rounded-xl flex-1 outline-none border-0 p-0 ring-0 focus:ring-0 text-black100"
-            type="text"
-          />
-        </div>
-        <div className="flex flex-col gap-4">
-          <span className="max-w-min font-mavenPro text-2xl border-b text-[#5EAF6B]">
-            SENHA:
-          </span>
-          <input
-            className="w-full px-3 w-72 py-6 bg-[#111111]/85 rounded-xl flex-1 outline-none border-0 p-0 text-sm ring-0 focus:ring-0"
-            type="text"
-          />
-        </div>
-        
-        <div className="flex justify-between text-xl tracking-wider">
-          <div className="flex gap-4 items-center">
-            <input
-              className="size-4 bg-black//20 rounded border-4 border-white/50"
-              type="checkbox"
-            />
-            <span>Deseja se manter conectado?</span>
-          </div>
-          <div>
-            <span>
-              Esqueceu a senha? <a href="#">Clique aqui!</a>
-            </span>
-          </div>
-        </div>
-        <div className="w-full pt-14 flex justify-around">
-          <div className="w-1/3">
-            <button className="w-full p-6 rounded-2xl text-black500 text-2xl border-4 ">
-              Entrar com o Google G
-            </button>
-          </div>
-          <div className="w-1/3">
-            <button className="w-full p-6 rounded-2xl bg-[#5EAF6B] text-white text-2xl">
-              Entrar
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    <>
+      <SiteInstitucional />
+    </>
+  )
 }
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,48 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import WebFont from 'webfontloader';
+
 import "./index.css"
-import { App } from './App';
-import { LoginPage } from './pages/Login/LoginPage';
+
+// import Root from "@routes/Root";
+// import ErrorPage from "@routes/errorPage";
+// import LoginRoute  from "@routes/LoginRoute" 
+
+import CadastroPage from '@pages/Cadastro/cadastroPage';
+
+// import {
+//   createBrowserRouter,
+//   RouterProvider,
+// } from "react-router-dom";
+
+WebFont.load({
+  google: {
+     families: [
+     'Inter:100,200,300,400,500,600,700,800,900', 
+     'Maven-pro:400,500,600,700,800,900',
+     'Megrim',
+    ]
+  }
+})
+
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Root />,
+//     errorElement: <ErrorPage />
+//   },
+//   {
+//     path: "login",
+//     element: <LoginRoute />,
+//     errorElement: <ErrorPage />,
+//   }, 
+// ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <LoginPage />
+    <div className='font-inter font-normal bg-black500 m-0 min-w-80 min-h-screen'>
+    <CadastroPage />
+    </div>
   </React.StrictMode>
 );
