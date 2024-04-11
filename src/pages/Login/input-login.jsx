5dd50d60bd51cc873c20f75f7c27472ddca01544
UmSamu <samuel.silva@sptech.school>
2024-04-11 20:01:58 -0300

    refactor: plotando loginPage na tela

--- src/pages/Login/input-login.jsx
+++ src/pages/Login/input-login.jsx
@@ -3,7 +3,7 @@ export function InputLogin(props) {
         <div className="flex flex-col gap-4">
           <span className="max-w-min font-mavenPro text-2xl border-b text-[#5EAF6B]" {...props} />
           <input
-            className="w-full px-3 w-72 py-6 bg-[#111111]/85 rounded-xl flex-1 outline-none border-0 p-0 ring-0 focus:ring-0 text-black100"
+            className="w-full px-3 w-72 py-6 bg-[#111111]/85 rounded-xl flex-1 outline-none text-black100"
             type="text"
           />
         </div>
