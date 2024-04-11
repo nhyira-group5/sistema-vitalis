0122b3c4b53a1534249897275031684ca47916df
Cauã <cauaempr@gmail.com>
2024-04-11 16:46:55 -0300

    feat - desenvolvimento tela de cadastro

--- src/components/LoginRegisterCard/loginRegisterCard.jsx
+++ src/components/LoginRegisterCard/loginRegisterCard.jsx
@@ -3,21 +3,21 @@ import LogoLilasBlackNormalSize from "@assets/logos/lilasNormalSizeblackColor.sv
 
 const LoginRegisterCard = ({tittle, contentText, userType, Button}) =>{
     return(
-        <div className="flex flex-col p-14 bg-white text-black500  items-center text-center w-1/3 h-full gap-36">
+        <div className="flex flex-col p-14 bg-white text-black500  items-center text-center w-1/3 h-full justify-between">
             <div className="flex flex-col items-center justify-center  gap-16">
                 {userType ? 
-                (<img className="w-3/5" src={LogoGreenBlackNormalSize} alt="Logo vitalis"/>) 
-                : (<img className="w-3/5" src={LogoLilasBlackNormalSize} alt="Logo vitalis"/>)}
+                (<img className="w-4/5" src={LogoGreenBlackNormalSize} alt="Logo vitalis"/>) 
+                : (<img className="w-4/5" src={LogoLilasBlackNormalSize} alt="Logo vitalis"/>)}
                 
-                <span className="text-4xl font-bold tracking-widest">{tittle}</span>
-                <span className="text-lg">{contentText}</span>
+                <span className="text-2xl font-bold tracking-widest">{tittle}</span>
+                <span className="text-sm">{contentText}</span>
             </div>
 
             {Button}
 
-            <h5>
+            <span className="text-xs">
             © 2024 nhyira. All Rights reserved
-            </h5>
+            </span>
             
         </div>
     )
--- src/pages/Cadastro/cadastroPage.jsx
+++ src/pages/Cadastro/cadastroPage.jsx
@@ -1,11 +1,12 @@
 import LoginRegisterCard from "@components/LoginRegisterCard/loginRegisterCard";
 import { useState } from "react";
 import Button from "@components/Button/button.jsx"
+import * as Tabs from '@radix-ui/react-tabs';
+import { ChevronLeftIcon, ChevronRightIcon} from '@radix-ui/react-icons'
+
 
 const CadastroPage = () =>{
-const [contentTexto] = useState("Já tem uma conta? Se sim, vamos lá! Se não, que tal criar uma agora? É fácil e rápido. Vamos nessa?");
-const [tittle] = useState("Já tem uma conta?");
-const [buttonContent] = useState("Entrar");
+
 const [userType, setUserType] = useState(true)
 
 
@@ -18,10 +19,39 @@ const handleUserTypeChange = () => {
 return(
 <>
     <div className="flex h-screen w-screen bg-black500 ">
-        <LoginRegisterCard tittle={tittle} 
-                           contentText={contentTexto} 
+        <LoginRegisterCard tittle={`${userType ? 'É um instrutor?' : 'É um aluno?'}`} 
+                           contentText={`${userType ? 'macarao com sasicha' : 'socoro jesus'}`} 
                            userType = {userType} 
-                           Button={<Button onClick={handleUserTypeChange} style={`text-lg px-8 py-3 rounded-xl tracking-[.3rem] text-white font-bold ${userType ? 'bg-primary-green300' : 'bg-alt-purple300'}`} content={buttonContent} />}/>
+                           Button={<Button onClick={handleUserTypeChange} style={`text-md px-7 py-3 rounded-xl tracking-[0.2rem] text-white font-bold ${userType ? 'bg-primary-green300' : 'bg-alt-purple300'}`} content={`${userType ? 'Instrutor' : 'Aluno'}`} />}/>
+
+        <div className="flex flex-col gap-4 text-white p-14 pt-20 w-full">
+            <div className="flex flex-col gap-3">
+                <span className="text-5xl font-bold text-primary-green300">Realizando cadastro</span>
+
+                <span className="mt-3 text-sm font-light">Quer acessar nossa aplicação? Vamos realizar seu cadastro!</span>
+                <span className="text-sm font-light">Insira algumas informações sobre você para fazermos o cadastro de sua conta!</span>
+            </div>
+
+        
+            <Tabs.Root className="w-full flex flex-col items-center" defaultValue="tab1">
+                <Tabs.List className="flex gap-6"  aria-label="tabs">
+                    <Tabs.Trigger value="tab1"><ChevronLeftIcon /></Tabs.Trigger>
+                     <Tabs.Trigger className="border-2 px-2 rounded-full" value="tab1">1</Tabs.Trigger>
+                     <Tabs.Trigger className="border-2 px-2 rounded-full" value="tab2">2</Tabs.Trigger>
+                    <Tabs.Trigger value="tab2"><ChevronRightIcon /></Tabs.Trigger>
+                </Tabs.List>
+
+                <Tabs.Content  value="tab1">
+
+
+                </Tabs.Content>
+
+
+                <Tabs.Content value="tab2">Tab two content</Tabs.Content>
+
+            </Tabs.Root>
+        
+        </div>
     </div>
 </>
 )
