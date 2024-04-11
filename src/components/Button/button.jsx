5173f1c01523d92e4b14df03e95b56b45f4feb72
Cauã <cauaempr@gmail.com>
2024-04-11 13:37:59 -0300

    feata - componente Button e loginRegisterCard

---
+++ src/components/Button/button.jsx
@@ -0,0 +1,11 @@
+const Button = ({ onClick, style, content }) => {
+    return (
+      <>
+        <button onClick={onClick} className={style}>
+          {content}
+        </button>
+      </>
+    );
+  };
+  
+  export default Button;
--- src/components/LoginRegisterCard/loginRegisterCard.jsx
+++ src/components/LoginRegisterCard/loginRegisterCard.jsx
@@ -1,21 +1,24 @@
 import LogoGreenBlackNormalSize from "@assets/logos/greenNormalSizeBlackColor.svg"
+import LogoLilasBlackNormalSize from "@assets/logos/lilasNormalSizeblackColor.svg"
 
-const LoginRegisterCard = ({tittle, contentText, buttonContent, userType, type}) =>{
+const LoginRegisterCard = ({tittle, contentText, userType, Button}) =>{
     return(
-        <div className="flex flex-col p-4 bg-white text-black500 justify-center items-center text-center w-1/3 h-full">
-
-            <div className="flex flex-col items-center justify-center   ">
-                <img className="w-2/4" src={LogoGreenBlackNormalSize} alt="Logo vitalis"/>
-                <h1>{tittle}</h1>
-                <h4>{contentText}</h4>
+        <div className="flex flex-col p-14 bg-white text-black500  items-center text-center w-1/3 h-full gap-36">
+            <div className="flex flex-col items-center justify-center  gap-16">
+                {userType ? 
+                (<img className="w-3/5" src={LogoGreenBlackNormalSize} alt="Logo vitalis"/>) 
+                : (<img className="w-3/5" src={LogoLilasBlackNormalSize} alt="Logo vitalis"/>)}
+                
+                <span className="text-4xl font-bold tracking-widest">{tittle}</span>
+                <span className="text-lg">{contentText}</span>
             </div>
 
-            <button>{buttonContent}</button>
+            {Button}
 
             <h5>
             © 2024 nhyira. All Rights reserved
             </h5>
-            but
+            
         </div>
     )
 }
