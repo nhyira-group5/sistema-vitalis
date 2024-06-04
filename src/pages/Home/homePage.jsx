2605204fe158159c33bd0d61514a53d17461c5cf
augustoSamu <samuel.sival@sptech.school>
2024-06-04 20:27:35 -0300

    fix: homePage

--- src/pages/Home/homePage.jsx
+++ src/pages/Home/homePage.jsx
@@ -49,7 +49,6 @@ export function HomePage() {
   );
 
   useEffect(() => {
-
     generateCurrentyAmountExercises();
     generateCurrentyAmountMeals();
     generateCurrentyAmountDays();
@@ -70,8 +69,6 @@ export function HomePage() {
     [activitySelected],
     [activityInformation]
   );
-
-
 
   function handleSelectActivity(e) {
     setActivityType(e.type);
@@ -142,9 +139,8 @@ export function HomePage() {
     setCurrentyAmountDays(currentyDays);
   }
 
-
+  // GERAR ATIVIDADES DO DIA
   function generateActivitiesDay() {
-
     const activityDays = listaSemanal.find((element) => !element.concluido);
 
     if (activityDays) {
@@ -170,7 +166,6 @@ export function HomePage() {
     if (lastInputRef.current) {
       lastInputRef.current.focus();
     }
-
   }
 
   const nickname = "Squirte";
@@ -283,37 +278,14 @@ export function HomePage() {
 
   return (
     <div className="w-full h-screen flex justify-evenly items-center bg-[#F7FBFC]">
-    <SideBar />
-    <div className="w-[88%] h-[90%] flex flex-col justify-between">
-      <h1 className="text-[#2B6E36] font-semibold text-2xl">Home</h1>
-      <div className="w-full h-[93%] flex justify-between items-center">
-        <div className="w-[34%] h-full flex flex-col justify-between">
-          <div className="w-full h-[5%] font-semibold text-xl flex justify-center items-center rounded-xl">
-            <h1>Bem-vindo(a), {nicknameUser}</h1>
-          </div>
-          <div className="w-full h-[72%] bg-white shadow-lg flex flex-col justify-between items-center rounded-xl p-4">
-            <h2 className="w-full font-semibold">Atividades de hoje</h2>
-            <div className="w-full h-[31%] bg-black flex justify-between items-center rounded-3xl p-4">
-              <AtividadeCard
-                icon={<BowlSteam size={28} />}
-                title="Refeições"
-                currentAmount={currentyAmountMeals}
-                totalAmount={totalAmountMeals}
-              />
-              <AtividadeCard
-                icon={<Barbell size={28} />}
-                title="Exercícios"
-                currentAmount={currentyAmountExercises}
-                totalAmount={totalAmountExercises}
-              />
-              <AtividadeCard
-                icon={<CalendarCheck size={28} />}
-                title="Meta de dia"
-                currentAmount={currentyAmountDays}
-                totalAmount={totalAmountDays}
-              />
+      <SideBar />
+      <div className="w-[88%] h-[90%] flex flex-col justify-between">
+        <h1 className="text-[#2B6E36] font-semibold text-2xl">Home</h1>
+        <div className="w-full h-[93%] flex justify-between items-center">
+          <div className="w-[34%] h-full flex flex-col justify-between">
+            <div className="w-full h-[5%] font-semibold text-xl flex justify-center items-center rounded-xl">
+              <h1>Bem-vindo(a), {nicknameUser}</h1>
             </div>
-
             <div className="w-full h-[72%] bg-white shadow-lg flex flex-col justify-between items-center rounded-xl p-4">
               <h2 className="w-full font-semibold">Atividades de hoje</h2>
               <div className="w-full h-[31%] bg-black flex justify-between items-center rounded-3xl p-4">
@@ -380,12 +352,10 @@ export function HomePage() {
                 onClickFunction={() => completedActivity()}
               />
             )}
-
           </div>
-          <div className="w-full h-[16%] bg-white text-sm shadow-lg flex justify-between items-center rounded-xl p-4">
-            <h1 className="w-[60%] text-wrap ">
-              Observe o seu resultado do seu esforço com o seu
-              <span className="font-bold"> mural de fotos!</span>
+          <div className="w-[20%] h-full bg-[#1A1A1A] flex flex-col justify-between items-center rounded-xl shadow-lg p-4">
+            <h1 className="w-full text-white text-xl font-semibold flex items-center justify-center">
+              Lembretes
             </h1>
             <div className="w-full h-5/6  flex flex-col gap-2 overflow-hidden overflow-y-scroll">
               {reminders.length > 0 ? (
@@ -403,44 +373,10 @@ export function HomePage() {
               onClick={createReminder}
             >
               Adicionar lembrete
-
             </button>
           </div>
         </div>
-        <div className="w-[40%] h-full bg-white flex flex-col justify-center items-center rounded-xl shadow-lg p-4">
-          {!activitySelected ? (
-            <span className="h-full w-full flex justify-center items-center font-medium text-xl">
-              Escolha uma atividade
-            </span>
-          ) : (
-            <ExercicioBoard
-              activitySelected
-              type={activityType}
-              name={activityName}
-              description="Esta refeição oferece uma combinação equilibrada de nutrientes e benefícios que favorecem a perda de peso. A quinoa e os legumes grelhados são baixos em calorias e ricos em fibras, proporcionando saciedade e controlando o apetite. Além disso, são fontes de nutrientes essenciais, como proteínas, vitaminas e minerais, importantes para manter a saúde durante o processo de perda de peso. Ao ser preparadbusca emagrecer."
-            />
-          )}
-        </div>
-        <div className="w-[20%] h-full bg-[#1A1A1A] flex flex-col justify-between items-center rounded-xl shadow-lg p-4">
-          <h1 className="w-full text-white text-xl font-semibold flex items-center justify-center">
-            Lembretes
-          </h1>
-          <div className="w-full h-5/6  flex flex-col gap-2 overflow-hidden overflow-y-scroll">
-            <Message
-              text="Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy
-              text ever since the 1500s, when an unknown printer took a galley
-              of type and scrambled it to make a type specimen book. It has
-              survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged."
-            />
-          </div>
-          <button className="px-9 py-2 rounded-2xl shadow-lg text-sm text-white bg-[#48B75A]">
-            Adicionar lembrete
-          </button>
-        </div>
       </div>
     </div>
-  </div>
   );
 }
