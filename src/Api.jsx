4620727b113509fec15c94d5b7a78f7cb671b860
WillDantasJPG <willgustavodantasadolpho@gmail.com>
2024-10-08 19:50:24 -0300

    feature

--- src/Api.jsx
+++ src/Api.jsx
@@ -2,10 +2,12 @@ import axios from 'axios';
 
 export const api = axios.create({
   baseURL: 'http://localhost:8080',
+  //baseURL: "http://34.231.202.208:5225" // IP publico do backend
 });
 
 export const apiChat = axios.create({
   baseURL: 'http://localhost:3001',
+  //const URL = 'http://44.196.0.230:3001';
 });
 
 export function USER_GET(id) {
