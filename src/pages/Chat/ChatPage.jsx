fe7df2f22a63ec0d67438858eb4def6f19ce1670
Will Gustavo Dantas <71149012+WillDantasJPG@users.noreply.github.com>
2024-10-10 22:04:54 -0300

    feature:chatpage

--- src/pages/Chat/ChatPage.jsx
+++ src/pages/Chat/ChatPage.jsx
@@ -32,7 +32,7 @@ export function ChatPage() {
     if (!user) return;
     
     // socket.current = io("http://localhost:3001");
-    socket.current = io("http://44.223.87.21:3001");
+    socket.current = io("http://3.226.245.89:3001");
 
     socket.current.on("receive_message", (data) => {
       handleMessageReceived(data);
