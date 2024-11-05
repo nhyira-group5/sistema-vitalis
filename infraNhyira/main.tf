00261226f3df92473efbfe503a7adff76d8f4eb3
Will Gustavo Dantas <71149012+WillDantasJPG@users.noreply.github.com>
2024-11-05 04:07:34 -0300

    Update main.tf

--- infraNhyira/main.tf
+++ infraNhyira/main.tf
@@ -47,7 +47,7 @@ sudo mkdir -p /home/ubuntu/frontend
 
  # Baixar a versão mais recente do Docker Compose
 DOCKER_COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep -oP '"tag_name": "\K[^\"]+')
-sudo curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
 sudo chmod +x /usr/local/bin/docker-compose
 
 # Instalar Certbot
