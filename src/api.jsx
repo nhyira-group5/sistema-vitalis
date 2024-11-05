5b90e776032c3db6068cdb592b4ae71d5a0f7d1a
Will Gustavo Dantas <71149012+WillDantasJPG@users.noreply.github.com>
2024-11-05 02:46:49 -0300

    Update api.jsx

--- src/api.jsx
+++ src/api.jsx
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://34.231.202.208:5225",
+  baseURL: "http://18.211.206.217:5225",
 });
 
 export const apiChat = axios.create({
-  baseURL: "http://54.164.44.70:3001",
+  baseURL: "http://54.160.38.83:3001",
 });
 
 export function USER_GET(id) {
