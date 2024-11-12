d815d6a37da640ea7d2b25384cf2158ea1ce4c8b
Will Gustavo Dantas <71149012+WillDantasJPG@users.noreply.github.com>
2024-11-12 19:02:00 -0300

    Update api.jsx

--- src/api.jsx
+++ src/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://vitalis-uat.zapto.org:5225",
+  baseURL: "http://18.211.206.217:5225",
 });
 
 export const apiChat = axios.create({
