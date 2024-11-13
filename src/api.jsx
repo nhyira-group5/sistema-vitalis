d21c00ec29366d69931b88106213f2ba9300830f
Will Gustavo Dantas <71149012+WillDantasJPG@users.noreply.github.com>
2024-11-12 23:39:12 -0300

    Update api.jsx

--- src/api.jsx
+++ src/api.jsx
@@ -1,13 +1,15 @@
 import axios from "axios";
 
+// Define o domínio do frontend como API_URL
+const API_URL = "https://vitalis-uat.zapto.org";
+
 export const api = axios.create({
-  baseURL: "http://18.211.206.217:5225",
+  baseURL: API_URL, // Define a URL base para `api`
 });
 
 export const apiChat = axios.create({
-  baseURL: "http://54.160.38.83:3001",
+  baseURL: "http://54.160.38.83:3001", // Mantém o IP para o chat
 });
-
 export function USER_GET(id) {
   return {
     url: API_URL + "/usuarios/" + id,
